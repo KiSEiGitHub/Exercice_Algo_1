067412c89831998568f4ad8f857025508886b025
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-05 10:12:35 +0200

    1 + 2 + 3 + ... +100

---
+++ Python - For While If/Exercice 1/a.py
@@ -0,0 +1,13 @@
+def main():
+    a = 0
+    for i in range(1, 101):
+        a += i
+    print(a)
+
+
+def sansboucle():
+    a = sum(range(1, 101))
+    print(a)
+
+if __name__ == '__main__':
+    main()
