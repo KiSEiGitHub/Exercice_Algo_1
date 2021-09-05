0e2ccc0393039830b8e26092f8f1704945b3d307
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-05 08:57:08 +0200

    Un résultat surprenant!

--- Python - Boucle while - Tant que/Exercice 1.py
+++ Python - Boucle while - Tant que/Exercice 1.py
@@ -1,8 +1,13 @@
-feuille = 0.1
-x = 0
+def main():
+    feuille = 0.1
+    x = 0
 
-while feuille <= 324000:
-    x += 1
-    feuille *= 2
+    while feuille <= 324000:
+        x += 1
+        feuille *= 2
 
-print(f"Il faudra plier la feuille {feuille} fois")
+    print(f"Il faudra plier la feuille {feuille} fois")
+
+
+if __name__ == '__main__':
+    main()
